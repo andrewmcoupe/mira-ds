d588e8f44d150e38d066a7ebe6a8f20a4cb6ba12
Andy Coupe <andrewmcoupe@gmail.com>
2022-04-24 08:27:42 +0100

    remove rc

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,4 @@
-const withTM = require("next-transpile-modules")(["component-library"]);
+const withTM = require("next-transpile-modules")(["@mira-ds/components"]);
 
 module.exports = withTM({
   reactStrictMode: true,
