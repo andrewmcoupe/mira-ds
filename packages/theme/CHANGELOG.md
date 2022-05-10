62ce7446bbb0b3dd07a1764f80454d4656e80c1d
Andy Coupe <andrewmcoupe@gmail.com>
2022-05-10 07:05:07 +0100

    add theme provider

---
+++ packages/theme/CHANGELOG.md
@@ -0,0 +1,7 @@
+# @mira-ds/theme-provider
+
+## 0.0.1
+
+### Patch Changes
+
+- Add theme provider
---
+++ packages/theme/src/ThemeProvider.tsx
@@ -0,0 +1,42 @@
+import { ThemeProvider, createGlobalStyle } from "styled-components";
+import { WithChildren } from "@mira-ds/components/src/utils/types";
+import { theme } from "./theme";
+
+const DEFAULT_THEME = theme;
+
+const GlobalStyle = createGlobalStyle`
+  * { 
+    margin: 0; 
+    font-family: "$poppins" ,sans-serif 
+  }
+
+  *::before {
+     box-sizing: border-box;
+  }
+
+  *::after {
+    box-sizing: border-box;
+  }
+
+  body {
+    line-height: 1.5;
+    -webkit-font-smoothing: antialiased;
+  }
+  
+  img, picture, svg, video, canvas {
+    display: block;
+    max-width: 100%;
+  }
+`;
+
+const MiraThemeProvider = ({
+  children,
+  theme,
+}: WithChildren<{ theme: {} }>) => (
+  <>
+    <GlobalStyle />
+    <ThemeProvider theme={theme ?? DEFAULT_THEME}>{children}</ThemeProvider>
+  </>
+);
+
+export { MiraThemeProvider, GlobalStyle, createGlobalStyle };
