b1f2b2336beb3c17ab3455092ecc7e073c0e5549
Andy Coupe <andrewmcoupe@gmail.com>
2022-06-05 07:36:13 +0100

    tidy up web app

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,18 +1,10 @@
 import { Button } from "@mira-ds/components";
-import { MiraThemeProvider } from "@mira-ds/theme-provider";
-import styled from "styled-components";
-
-const Foo = styled(Button)`
-  background: ${({ theme }) => theme.colors.blue10};
-  ${({ theme }) => theme.utils.px(24)}
-`;
 
 export default function Web() {
   return (
-    <MiraThemeProvider>
-      <Foo>Hiiii</Foo>
+    <>
       <h1>Web</h1>
       <Button />
-    </MiraThemeProvider>
+    </>
   );
 }
